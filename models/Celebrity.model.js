654717bd9e0f35c978fddcc78425b84159254bfe
Michael Hodgkins <michaelhodgkins@Michaels-MacBook-Pro.local>
2022-08-15 11:50:06 +0100

    Iteration 2 done

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,15 @@
 //  Add your code here
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const celebSchema = new Schema(
+        {
+            name: String,
+            occupation: String,
+            catchPhrase: String
+
+
+        }
+);
+
+module.exports = model('Celeb', celebSchema);
