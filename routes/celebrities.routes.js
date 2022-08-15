22dbc5da4ae0dbf036ccba24897e0d112f305a19
Michael Hodgkins <michaelhodgkins@Michaels-MacBook-Pro.local>
2022-08-15 14:34:20 +0100

    iteration 3 done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -2,5 +2,17 @@
 const router = require("express").Router();
 
 // all your routes here
+const Celeb = require('../models/Celebrity.model');
+
+router.get('/create', (req, res) => res.render('celebrities/new-celebrity'));
+
+router.post('/create', (req,res,next) => {
+    console.log(req.body);
+    const { name, occupation, catchPhrase } = req.body;
+
+    Celeb.create({ name, occupation, catchPhrase })
+        .then(() => res.redirect('/'))
+        .catch((err) =>`Your celebrity cannot be made ${err} `);
+})
 
 module.exports = router;
